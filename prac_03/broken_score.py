82845165830a3898aff318668285051be14b6050
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-07-31 19:31:16 +0800

    Modify functionality

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,9 +1,22 @@
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif 50 < score <= 90:
-    print("Passable")
-elif score > 90:
-    print("Excellent")
-else:
-    print("Bad")
+def main():
+    score = get_score()
+    check_score(score)
+
+
+def check_score(score):
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif 50 < score <= 90:
+        print("Passable")
+    elif score > 90:
+        print("Excellent")
+    else:
+        print("Bad")
+
+
+def get_score():
+    score = float(input("Enter score: "))
+    return score
+
+
+main()
