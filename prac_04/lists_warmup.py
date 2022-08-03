27b6df684cbb21a9a2e24690307db7c0e0a7c138
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-03 11:27:16 +0800

    Get all the elements from numbers except the first two (slice)

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -13,7 +13,7 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 
 numbers[0] = "ten" # Change the first element of numbers to "ten" (the string, not the number 10)
 numbers[-1] = 1 # Change the last element of numbers to 1
-
+numbers[2:] # Get all the elements from numbers except the first two (slice)
 
 
 
