a72f1821b1c089eeab0a935a916b99a5caeeb799
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-03 12:26:41 +0800

    Add lines to create a list of integers

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -35,7 +35,8 @@ print(lowercase_full_names)
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 # TODO: use a list comprehension to create a list of integers
 # from the above list of strings
-# numbers =
+numbers = [int(almost_number) for almost_number in almost_numbers]
+print(numbers)
 
 # TODO: use a list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
