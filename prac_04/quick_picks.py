1c7682b886a3bae3364f4d76a6b9f71965ecee8d
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-03 12:36:09 +0800

    (Correction) Initial Commit

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -5,7 +5,7 @@ def main():
     numbers_of_quick_picks = int(input("How many quick picks? "))
     for i in range(numbers_of_quick_picks):
         quick_pick = []
-        for j in range(6):
+        for j in range(6): # numbers for each line is 6
             number = random.randint(1, 45)
             quick_pick.append(number)
         print(quick_pick)
