c5dfe026fdfc56c999c71c88e2a80ff51f3e08ef
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-03 12:08:31 +0800

    Add lines to display result

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -4,5 +4,11 @@ def main():
         input_numbers = int(input("Numbers: "))
         numbers.append(input_numbers)
 
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The average of the numbers is {sum(numbers) / len(numbers) }")
+
 
 main()
