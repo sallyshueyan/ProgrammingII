c786c125d43905668fac6e709e095d4674974b24
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-03 11:29:56 +0800

    Check if 9 is an element of numbers

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -11,9 +11,8 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 
 
-numbers[0] = "ten" # Change the first element of numbers to "ten" (the string, not the number 10)
-numbers[-1] = 1 # Change the last element of numbers to 1
-numbers[2:] # Get all the elements from numbers except the first two (slice)
-
-
-
+numbers[0] = "ten"  # Change the first element of numbers to "ten" (the string, not the number 10)
+numbers[-1] = 1  # Change the last element of numbers to 1
+numbers[2:]  # Get all the elements from numbers except the first two (slice)
+# Check if 9 is an element of numbers
+9 in numbers  # True
