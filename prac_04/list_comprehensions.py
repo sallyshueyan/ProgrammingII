ffc0795da12d118853e2ad80ccf588b8ee8b7360
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-03 12:28:42 +0800

    Add lines to create a list of only the numbers that are

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -40,3 +40,5 @@ print(numbers)
 
 # TODO: use a list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
+big_numbers = [number for number in numbers if number > 9]
+print(big_numbers)
