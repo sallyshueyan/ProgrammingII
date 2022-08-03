5506440a8261526992a2223e457643ff3cf00deb
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-03 12:25:28 +0800

    Add lines to create a list of all of the full_names

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -29,7 +29,8 @@ print(a_names)
 
 # TODO: use a list comprehension to create a list of all of the full_names
 # in lowercase format
-# lowercase_full_names =
+lowercase_full_names = [name.lower() for name in full_names]
+print(lowercase_full_names)
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 # TODO: use a list comprehension to create a list of integers
