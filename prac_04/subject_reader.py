52404a8b4b09f339a023f0ec7a72496de8c54f9a
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-03 11:53:08 +0800

    Store data, return in a list

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,11 +8,12 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    #print(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -22,8 +23,10 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
-        print("----------")
+        #print("----------")
+        data.append(parts)
     input_file.close()
+    return data
 
 
 main()
