229b76ba9e976510c550415e1700afa7ae5de1e7
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-05 09:52:08 +0800

    Add new version of (Basic list operations)

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -19,6 +19,31 @@ def display_result(numbers):
 
 main()
 
+
+# Second Version (1. Basic list operations)
+def main():
+    numbers = []  # collect numbers as input in the list
+    number_count = 1
+    input_numbers = int(input(f"Numbers {number_count}: "))
+    while input_numbers > 0:
+        number_count += 1
+        input_numbers = int(input(f"Numbers {number_count}: "))
+        numbers.append(input_numbers)
+
+    display_result(numbers)
+
+
+def display_result(numbers):
+    """Display results in lines."""
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+
+main()
+
 # 2. Woefully inadequate security checker
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
