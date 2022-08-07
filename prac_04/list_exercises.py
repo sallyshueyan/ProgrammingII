d89884952a699d89044df2e83f1f166a198cda61
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-07 17:19:17 +0800

    Add function to request for numbers and exit loop when input is less than 0

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,49 +1,59 @@
 # 1. Basic list operations
-# def main():
-#     numbers = []  # collect numbers as input in the list
-#     for i in range(5):
-#         input_numbers = int(input("Numbers: "))
-#         numbers.append(input_numbers)
-#
-#     display_result(numbers)
-#
-#
-# def display_result(numbers):
-#     """Display results in lines."""
-#     print(f"The first number is {numbers[0]}")
-#     print(f"The last number is {numbers[-1]}")
-#     print(f"The smallest number is {min(numbers)}")
-#     print(f"The largest number is {max(numbers)}")
-#     print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+def main():
+    numbers = []  # collect numbers as input in the list
+    for i in range(5):
+        input_numbers = int(input("Numbers: "))
+        numbers.append(input_numbers)
+
+    display_result(numbers)
 
 
-#main()
+def display_result(numbers):
+    """Display results in lines."""
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
 
 
-# # Second Version (1. Basic list operations)
-# def main():
-#     numbers = []  # collect numbers as input in the list
-#     number_count = 1
-#     input_numbers = int(input(f"Numbers {number_count}: "))
-#     while input_numbers > 0:
-#         number_count += 1
-#         input_numbers = int(input(f"Numbers {number_count}: "))
-#         numbers.append(input_numbers)
-#
-#     display_result(numbers)
-#
-#
-# def display_result(numbers):
-#     """Display results in lines."""
-#     print(f"The first number is {numbers[0]}")
-#     print(f"The last number is {numbers[-1]}")
-#     print(f"The smallest number is {min(numbers)}")
-#     print(f"The largest number is {max(numbers)}")
-#     print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
-#
-#
-# main()
-#
+main()
+
+
+# Second Version (1. Basic list operations)
+def main():
+    numbers = []  # collect numbers as input in the list
+    number_count = 1
+    get_number(number_count, numbers)
+
+
+def get_number(number_count, numbers):
+    """Function request for input"""
+    number_validity = False
+    while number_validity is False:
+        input_numbers = int(input(f"Numbers {number_count}: "))
+        number_count += 1
+        numbers.append(input_numbers)
+        if input_numbers > 0:
+            input_numbers = int(input(f"Numbers {number_count}: "))
+            number_count += 1
+            numbers.append(input_numbers)
+        else:
+            number_validity = True
+    display_result(numbers)
+
+
+def display_result(numbers):
+    """Display results in lines."""
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The average of the numbers is {(sum(numbers) / len(numbers)):.2f}")
+
+
+main()
+
 # 2. Woefully inadequate security checker
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
