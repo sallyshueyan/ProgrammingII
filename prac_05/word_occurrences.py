e39b37a2b4501f945445bbdc5134ea2a8543e6ac
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-10 12:09:55 +0800

    Add a loop to check and count the repeated words.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,5 +1,14 @@
-word_to_count = {}
-text = input("Text: ")
-words = text.split()
+def main():
+    word_to_count = {}
+    text = input("Text: ")
+    words = text.split()
+
+    for word in words:
+        word_to_count[word] = word_to_count.get(word, 0) + 1
+
+    words = list(word_to_count.keys())
+    words.sort()
 
 
+
+main()
