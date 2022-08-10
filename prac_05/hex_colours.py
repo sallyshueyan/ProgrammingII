4bf8700cf96fbad58ad64ca1c2dfe6065513122d
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-10 11:47:36 +0800

    Add a constant dictionary of about 10 colour names.

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,6 @@
+COLOUR_NAME_TO_CODE = {"CandyAppleRed": '#ff0800', "DarkOrange": '#ff8c00', "GoldenYellow": '#ffdf00',
+                       "Inchworm": "#b2ec5d", "LightSkyBlue": "#87cefa",
+                       "PersianIndigo": "#32127a", "Purple": '#a020f0'}
+
+
+
