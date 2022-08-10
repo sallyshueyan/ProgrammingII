72a94e494b886d85cc5fa5a356243abd520b3a8b
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-10 11:53:42 +0800

    Add colours.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,8 +1,10 @@
-COLOUR_NAME_TO_CODE = {"CandyAppleRed": '#ff0800', "DarkOrange": '#ff8c00', "GoldenYellow": '#ffdf00',
-                       "Inchworm": "#b2ec5d", "LightSkyBlue": "#87cefa",
-                       "PersianIndigo": "#32127a", "Purple": '#a020f0'}
+def main():
+    COLOUR_NAME_TO_CODE = {"Puce": '#cc8899', "Pear": '#d1e231', "Light": '#eedd82',
+                           "Inchworm": "#b2ec5d", "LightSkyBlue": "#87cefa",
+                           "Lemon": "#fff700", "Purple": '#a020f0', "Khaki": "#f0e68c", "Jade": "#00a86b",
+                           "Iceberg": "#71a6d2"}
 
-colour_name = input("Enter a colour name: ").title()
+    colour_name = input("Enter a colour name: ").title()
 
 
-
+main()
