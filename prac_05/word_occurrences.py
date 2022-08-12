3c9eb589463011f26759077e24c6ebfeaa28eaf5
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-12 11:23:27 +0800

    Reformat Code.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -10,6 +10,7 @@ def main():
     words.sort()
 
     def check_alignment():
+        """Check alignment."""
         length_of_word = 0
         for word in word_to_count:
             if length_of_word < len(word):
