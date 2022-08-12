ca358236adec43589376cd9166f855a6bf59e25b
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-12 10:49:40 +0800

    Add lower case conversion.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,16 +1,16 @@
 def main():
-    COLOUR_NAME_TO_CODE = {"Puce": '#cc8899', "Pear": '#d1e231', "Light": '#eedd82',
-                           "Inchworm": "#b2ec5d", "LightSkyBlue": "#87cefa",
-                           "Lemon": "#fff700", "Purple": '#a020f0', "Khaki": "#f0e68c", "Jade": "#00a86b",
-                           "Iceberg": "#71a6d2"}
+    COLOUR_NAME_TO_CODE = {"puce": '#cc8899', "pear": '#d1e231', "light": '#eedd82',
+                           "inchworm": "#b2ec5d", "lightSkyBlue": "#87cefa",
+                           "lemon": "#fff700", "purple": '#a020f0', "khaki": "#f0e68c", "jade": "#00a86b",
+                           "iceberg": "#71a6d2"}
 
-    colour_name = input("Enter a colour name: ").title()
+    colour_name = input("Enter a colour name: ").lower()
     while colour_name != "":
         if colour_name in COLOUR_NAME_TO_CODE:
             print(colour_name, "is", COLOUR_NAME_TO_CODE[colour_name])
         else:
             print("Invalid colour name")
-        colour_name = input("Enter a colour name: ").title()
+        colour_name = input("Enter a colour name: ").lower()
 
 
 main()
