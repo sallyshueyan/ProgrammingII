17729f64b458ff2e147a3162e200b7a86e3138e3
isaacwonghy <110077506+isaacwonghy@users.noreply.github.com>
2022-08-12 14:35:21 +0800

    added comments
    added comments @sallyshueyan

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -4,10 +4,10 @@ def main():
                            "lemon": "#fff700", "purple": '#a020f0', "khaki": "#f0e68c", "jade": "#00a86b",
                            "iceberg": "#71a6d2"}
 
-    colour_name = input("Enter a colour name: ").lower()
+    colour_name = input("Enter a colour name: ").lower() #ask user for colour name
     while colour_name != "":
-        if colour_name in COLOUR_NAME_TO_CODE:
-            print(colour_name, "is", COLOUR_NAME_TO_CODE[colour_name])
+        if colour_name in COLOUR_NAME_TO_CODE: 
+            print(colour_name, "is", COLOUR_NAME_TO_CODE[colour_name]) #formatting to print colour code
         else:
             print("Invalid colour name")
         colour_name = input("Enter a colour name: ").lower()
