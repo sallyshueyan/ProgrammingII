4f984c824911682bd84d2b9eff3d85411cac203e
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-12 09:18:27 +0800

    Add function to check username.

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,5 +2,15 @@ def main():
     email_to_name = {}
     user_email = input("Email: ")
 
+    while user_email != "":
+        user_name = get_name_from_email(user_email)
+
+
+def get_name_from_email(user_email):
+    prefix = user_email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()
+    return name
+
 
 main()
