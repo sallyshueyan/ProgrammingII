86461a0902d61cd993b87f984d7f6cdafae86f43
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-24 11:41:22 +0800

    Initial Commit

---
+++ additionalprac/additionalprac_05/birthdate.py
@@ -0,0 +1,14 @@
+def main():
+    name_to_date_of_birth = {}
+
+    for i in range(2):
+        name = input(f"{i + 1}) Name? ")
+        date_of_birth = input("Birthday (dd/mm/yy)? ")  # string
+        data_of_birth = tuple([int(x) for x in date_of_birth.split("/")])
+        name_to_date_of_birth[name] = date_of_birth
+
+    print(name_to_date_of_birth)
+
+
+if __name__ == '__main__':
+    main()
