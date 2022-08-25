97e74cb8aa0f515c6c4a23caa7d209e4b455dfad
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-25 21:17:56 +0800

    Add loop to check inputs.

---
+++ prac_06/guitars.py
@@ -0,0 +1,9 @@
+from prac_06.guitar import Guitar
+guitars = []
+
+print("My guitars!")
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+
