386fc6ac40ea49d80ca27a94f50bc20126c64770
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-25 19:53:04 +0800

    Create a new Car object called "limo" that is initialised with 100 units of fuel.

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -15,6 +15,7 @@ def main():
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
 
+    limo = Car("Limo", 100)
+
 
 main()
-
