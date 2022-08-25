c7616a28d617d2f4b63e89d5cc19647038b12103
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-25 20:39:42 +0800

    Create a new list that contains these three existing ProgrammingLanguage objects.

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,6 +1,6 @@
 class ProgrammingLanguage:
 
-    def __init__(self,  name="", dynamic="", reflection="", year=""):
+    def __init__(self, name="", dynamic="", reflection="", year=0):
         self.name = name
         self.dynamic = dynamic
         self.reflection = reflection
@@ -9,5 +9,14 @@ class ProgrammingLanguage:
     def __str__(self):
         return f"{self.name}, {self.dynamic} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
+
 if __name__ == "__main__":
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(ruby.__str__())
+
+    languages = [ruby, python, visual_basic]
+    print(languages)
+
 
