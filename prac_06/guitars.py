25e1fde856e0b9d9b06c53df708ff37855dd50f2
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-25 22:21:04 +0800

    Add enumerate() function to display list according.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -12,8 +12,14 @@ def main():
         new_guitar = Guitar(name, year, cost)
         guitars.append(new_guitar)
         print(new_guitar, "added.")
+        name = input("Name: ")
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    print("These are my guitars:")
+
+    for i, guitar in enumerate(guitars):
+        print(f"Guitar{i}: {guitar.name:>21} ({guitar.year}), worth ${guitar.cost:>10}")
 
 
 main()
