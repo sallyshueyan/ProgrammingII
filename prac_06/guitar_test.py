faaaa63fccce06ff658f8eae473073e5f32cd65f
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-25 21:06:53 +0800

    Add function to check get_age() and is_vintage() functions works.

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,13 @@
+from prac_06.guitar import Guitar
+
+
+def main():
+    guitar1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    guitar2 = Guitar("Another Guitar", 2013, 12)
+    print(f"{guitar1.name} get_age() - Expected 98. Got {guitar1.get_age()}")
+    print(f"{guitar2.name} get_age() - Expected 7. Got {guitar2.get_age()}")
+    print(f"{guitar1.name} is_vintage() - Expected True. Got {guitar1.is_vintage()}")
+    print(f"{guitar2.name} is_vintage() - Expected False. Got {guitar2.is_vintage()}")
+
+
+main()
