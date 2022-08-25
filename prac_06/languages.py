309f44d819abb11bc6824836da97d593784b37a3
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-25 20:21:42 +0800

    Import the class.

---
+++ prac_06/languages.py
@@ -0,0 +1,10 @@
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+
+main()
