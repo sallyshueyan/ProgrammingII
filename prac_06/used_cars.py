afefa0a824a1122706f1456dced9946b402f7eeb
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-25 20:14:51 +0800

    Add names (literals) to the constructors Car objects.

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -6,7 +6,7 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("My Car", 180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
