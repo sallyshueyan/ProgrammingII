35e9bd00039a22a244f045c55d56dda34a647f38
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-25 20:29:35 +0800

    Add __str__ method to the class to return string.

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -5,3 +5,9 @@ class ProgrammingLanguage:
         self.dynamic = dynamic
         self.reflection = reflection
         self.year = year
+
+    def __str__(self):
+        return f"{self.name}, {self.dynamic} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+if __name__ == "__main__":
+
