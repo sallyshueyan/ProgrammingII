a8efbdbe84872a6db435080670d33c4afb768dec
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-25 20:26:31 +0800

    Add __init__ to define all attributes.

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,2 +1,7 @@
 class ProgrammingLanguage:
 
+    def __init__(self,  name="", dynamic="", reflection="", year=""):
+        self.name = name
+        self.dynamic = dynamic
+        self.reflection = reflection
+        self.year = year
