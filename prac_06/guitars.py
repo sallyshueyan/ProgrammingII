a07faa1fe764db16084c34dc17dc58549b9df464
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-25 22:03:41 +0800

    Collect new guitar information and append in a list.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,9 +1,19 @@
 from prac_06.guitar import Guitar
-guitars = []
 
-print("My guitars!")
-name = input("Name: ")
-while name != "":
-    year = int(input("Year: "))
-    cost = float(input("Cost: $"))
 
+def main():
+    guitars = []
+
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        print(new_guitar, "added.")
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+
+main()
