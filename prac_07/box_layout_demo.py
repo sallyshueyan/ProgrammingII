d4aebe66d0da9afe8c1248e752941e7649196546
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-08-31 21:32:50 +0800

    Add the handle_greet() function.

--- prac_07/box_layout_demo.py
+++ prac_07/box_layout_demo.py
@@ -8,5 +8,8 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        print("greet")
+
 
 BoxLayoutDemo().run()
