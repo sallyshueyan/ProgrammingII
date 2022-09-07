e80eb239f876764be81046f4956df44ab3805c18
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-09-07 11:47:02 +0800

    add a flagfall class variable set to $4.50.

--- prac_08/sliver_service_taxi.py
+++ prac_08/sliver_service_taxi.py
@@ -1,7 +1,11 @@
 from prac_08.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
+    flagfall = 4.5
+
     def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = fanciness * self.price_per_km
+
+
