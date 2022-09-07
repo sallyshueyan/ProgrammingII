77e3fdc6794ad4502de1929646a126c0354d4414
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-09-07 11:26:43 +0800

    Reformat Code.

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -1,15 +1,18 @@
 from prac_08.taxi import Taxi
 
+
 def main():
-# Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km
+    # Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km
     taxi = Taxi("Prius 1", 100)
-# Drive the taxi 40 km
+    # Drive the taxi 40 km
     taxi.drive(40)
-# Print the taxi's details and the current fare
+    # Print the taxi's details and the current fare
     print(taxi)
-# Restart the meter (start a new fare) and then drive the car 100 km
+    # Restart the meter (start a new fare) and then drive the car 100 km
     taxi.start_fare()
     taxi.drive(100)
-# Print the details and the current fare
+    # Print the details and the current fare
     print(taxi)
+
+
 main()
