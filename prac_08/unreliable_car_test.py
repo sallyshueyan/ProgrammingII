979cbac3057454ae9640bec468ac17b4bd20e58f
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-09-07 11:38:04 +0800

    Add variable for cars.

---
+++ prac_08/unreliable_car_test.py
@@ -0,0 +1,10 @@
+from prac_08.unreliable_car import UnreliableCar
+
+def main():
+    reliable_car = UnreliableCar("Car 1", 100, 99)
+    unreliable_car = UnreliableCar("Car 2", 100, 1)
+
+
+
+
+main()
