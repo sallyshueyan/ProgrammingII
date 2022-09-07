562f26b5c15373e9d46f61a3ee81d0f3902595bc
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-09-07 11:38:36 +0800

    Add loop to generate random number.

--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -4,6 +4,12 @@ def main():
     reliable_car = UnreliableCar("Car 1", 100, 99)
     unreliable_car = UnreliableCar("Car 2", 100, 1)
 
+    for i in range(1, 15):
+        print(f"Drive for {i}km:")
+        print(f"{reliable_car.name:5} drove {reliable_car.drive(i):2}km")
+        print(f"{unreliable_car.name:5} drove {unreliable_car.drive(i):2}km")
+
+
 
 
 
