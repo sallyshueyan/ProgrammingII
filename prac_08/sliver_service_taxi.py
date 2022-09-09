4cf33908d601dc8d140e197068e4929a26b3af78
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-09-09 10:20:10 +0800

    Add Docstring.

--- prac_08/sliver_service_taxi.py
+++ prac_08/sliver_service_taxi.py
@@ -1,15 +1,19 @@
 from prac_08.taxi import Taxi
 
+
 class SilverServiceTaxi(Taxi):
     flagfall = 4.5
 
     def __init__(self, name, fuel, fanciness):
+        """Construct main function."""
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = fanciness * self.price_per_km
 
-    def calculate_fare(self):
+    def get_fare(self):
+        """Function get fare."""
         return super().get_fare() + self.flagfall
 
     def __str__(self):
+        """Display details."""
         return f"{super().__str__()} plus flagfall of {self.flagfall}"
--- prac_08/sliver_service_taxi_test.py
+++ prac_08/sliver_service_taxi_test.py
@@ -2,6 +2,7 @@ from prac_08.sliver_service_taxi import SilverServiceTaxi
 
 
 def main():
+    """Create a new taxi with name, units of fuel and fare."""
     taxi = SilverServiceTaxi("Hummer", 100, 4)
     taxi.drive(20)
     print(taxi)
