b30e89a2dffb10a996841496420db4babf47c086
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-09-14 13:15:09 +0800

    Assign function to obtain input to assign files.

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -10,5 +10,19 @@ def main():
         if os.path.isdir(filename):
             continue
 
+        ext = get_extension(filename)
+        if ext not in ext_type_to_cat:
+            category = input("What category would you like to sort {} files into? ".format(ext))
+            ext_type_to_cat[ext] = category
+
+
+        os.rename(filename, "{}/{}".format(ext_type_to_cat[ext], filename))
+
+def get_extension(filename):
+    """Get the extension of target file."""
+    ext = filename.split('.')[-1]
+    return ext
+
+
 
 main()
