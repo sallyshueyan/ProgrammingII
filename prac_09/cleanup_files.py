d2bcd410e9e64696fbae9345b4568e29f57d5b3f
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-09-14 12:58:10 +0800

    Change variable name.

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -11,12 +11,10 @@ def main():
     print("Starting directory is: {}".format(os.getcwd()))
     os.chdir('Lyrics/Christmas')
     print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
-
     try:
         os.mkdir('temp')
     except FileExistsError:
         pass
-
     for filename in os.listdir('.'):
         if os.path.isdir(filename):
             continue
@@ -25,11 +23,11 @@ def main():
         print("Renaming {} to {}".format(filename, new_name))
 
 
-
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-    return new_name
+    new_title = ""
+    old_title = filename.replace(" ", "_").replace(".TXT", ".txt")
+    print(old_title)
 
 
 main()
