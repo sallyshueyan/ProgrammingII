e7c5b1305ef587699095149f18fa9e09f8f5cd33
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-09-21 10:56:07 +0800

    Uncomment the doctest.runmod() line to see how the doctests run.

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -57,7 +57,7 @@ run_tests()
 
 # TODO: 3. Uncomment the following line and run the doctests
 # (PyCharm may see your >>> doctest comments and run doctests anyway.)
-# doctest.testmod()
+doctest.testmod()
 
 # TODO: 4. Fix the failing is_long_word function
 # (don't change the tests, change the function!)
