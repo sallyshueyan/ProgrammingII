82064f706e2c11e59586d4c1372ed3ef93d17b27
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-09-23 10:37:19 +0800

    Display title, summary and url

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,1 +1,8 @@
+import wikipedia
+user_input = input("Enter a word or phrase to search: ")
+
+page = wikipedia.page(user_input, auto_suggest=False)
+print(f"Title  : {page.title}")
+print(f"Summary: {page.summary}")
+print(f"Url    : {page.url}")
 user_input = input("Enter a word or phrase to search: ")
