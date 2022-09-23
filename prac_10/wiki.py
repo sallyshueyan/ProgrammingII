a1938cff867f58cebfdf8c5c6b6d24821ae30998
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-09-23 10:38:20 +0800

    Add error checking.

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,9 +1,13 @@
 import wikipedia
 user_input = input("Enter a word or phrase to search: ")
-
-page = wikipedia.page(user_input, auto_suggest=False)
-print(f"Title  : {page.title}")
-print(f"Summary: {page.summary}")
-print(f"Url    : {page.url}")
-user_input = input("Enter a word or phrase to search: ")
+while user_input != '':
+    try:
+        page = wikipedia.page(user_input, auto_suggest=False)
+        print(f"Title  : {page.title}")
+        print(f"Summary: {page.summary}")
+        print(f"Url    : {page.url}")
+        user_input = input("Enter a word or phrase to search: ")
+    except wikipedia.PageError:
+        print(f"{user_input} result not found. Please try again")
+        user_input = input("Enter a word or phrase to search: ")
 print("Thank you and see you next time")
